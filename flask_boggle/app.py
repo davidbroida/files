5d87ed0da0d35e11c8f443b7d5d2b49c4f9748db
David Broida <87293379+davidbroida@users.noreply.github.com>
2021-12-06 12:51:13 +0800

    Add files via upload

---
+++ flask_boggle/app.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, request, session, jsonify
+from flask_debugtoolbar import DebugToolbarExtension
+from boggle import Boggle
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret-key'
+# app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
+debug = DebugToolbarExtension(app)
+
+boggle_game = Boggle()
+
+@app.route("/")
+def home_page():
+    return render_template("home.html")
+
+@app.route("/start")
+def start_game():
+    board = boggle_game.make_board()
+    session['board'] = board
+    highscore = session.get("highscore", 0)
+    return render_template("game.html", board = board, highscore = highscore)
+
+@app.route("/check-word")
+def check_word():
+    word = request.args["word"]
+    board = session["board"]
+    response = boggle_game.check_valid_word(board, word)
+    return jsonify({'result': response})
+
+@app.route("/post-score", methods = ["POST]"])
+def post_score():
+    score = request.json["score"]
+    highscore = session.get("highscore", 0)
+    
+    session['highscore'] = max(score, highscore);
+
+    return jsonify(brokeRecord = score > highscore)
